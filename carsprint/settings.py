936287ffe22f4d2e6cce28ed9cd17e274cfcb34a
sricharan-vanguru <vangurusricharan@gmail.com>
2021-07-16 22:44:30 +0530

    Completed Teams model

--- carsprint/settings.py
+++ carsprint/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carsprint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carsprint_db',
+        'USER': 'postgres',
+        'PASSWORD': 'charan',
+        'HOST': 'localhost'
     }
 }
 
@@ -119,8 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'carsprint/static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Team
+from django.utils.html import format_html
 # Register your models here.
+
+
+class Teamadmin(admin.ModelAdmin):
+  def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style ="border-radius: 50px;" />'.format(object.photo.url))
+  
+  thumbnail.short_description = 'Photo'
+  
+  list_display = ('id','first_name','designation','created_date',)
+  list_display_links = ('id','first_name',)
+  search_fields = ('first_name','last_name','designation',)
+
+admin.site.register(Team, Teamadmin)
+  
